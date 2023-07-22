50442f9ef3f651e22a67975166415a706d1dd70c
Jacob Alexander Thompson <jacobalthompson@gmail.com>
2023-07-21 17:21:19 -0700

    add pivot
    handle mouse clicks

--- src/pendulum_simulator/pendulum.py
+++ src/pendulum_simulator/pendulum.py
@@ -23,6 +23,11 @@ class Pendulum:
         self.black = pygame.Color(0, 0, 0)
         self.white = pygame.Color(255, 255, 255)
 
+        self.in_use = False
+
+        self.pivot = SCREEN_W >> 1, 50
+        self.pivot_radius = 7
+
     def set_window_properties(self):
         pygame.display.set_caption(TITLE)
 
@@ -42,6 +47,28 @@ class Pendulum:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.interact(event.pos, event.button)
+
+    def interact(self, pos, button):
+        pivot_hitbox = pygame.Rect(0, 0, 0, 0)
+        pivot_hitbox.center = self.pivot
+        pivot_hitbox.size = self.pivot_radius, self.pivot_radius
+
+        self.in_use = True
+
+        #button == 1 is mouse1
+        #button == 2 is scroll wheel
+        #button == 3 is mouse2
+
+        if button == 1:
+            if pivot_hitbox.collidepoint(pos): self.select_pivot()
+
+    def select_pivot(self):
+        print("frictionless pivot")
+        print(f"position: {self.pivot}")
+        print(f"radius: {self.pivot_radius}")
+
     def draw_background(self):
         self.surface.fill(self.white)
 
@@ -53,9 +80,15 @@ class Pendulum:
         dtrect = dtext.get_rect(bottomleft = dtpos)
         self.surface.blit(dtext, dtrect)
 
+    def draw_pivot(self):
+        pygame.draw.circle(self.surface, self.black, self.pivot, self.pivot_radius)
+
     def draw_frame(self):
         self.draw_background()
-        self.draw_info()
+        self.draw_pivot()
+
+        if not self.in_use:
+            self.draw_info()
 
     def update_frame(self):
         pygame.display.flip()
